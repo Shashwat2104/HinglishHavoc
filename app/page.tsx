dcae7e07cf545d344ec93a7a31b4369bafda9d7a
Shashwat Mahendra <shashwatmahender2104@gmail.com>
2025-06-04 19:45:17 +0530

    fixed bugs

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import ChatInterface from "@/components/ChatInterface";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100">
-      <div className="container mx-auto py-8">
+    <main >
+      <div >
         <ChatInterface />
       </div>
     </main>
